6eb7f965ec576e1684a661d4270763213ffe1012
SimplexComunicación <96500851+sebadipy@users.noreply.github.com>
2026-05-25 19:21:14 -0300

    Add files via upload

--- bot_asistente.js
+++ bot_asistente.js
@@ -685,8 +685,8 @@ class BotAsistente {
                     </div>
                     <div class="mobile-header-actions">
                         <button disabled title="Alquileres (Próximamente)"><i class="fa-solid fa-house-user"></i></button>
-                        <button type="button" title="Abrir Pagos" onclick="event.stopPropagation(); window.location.href='pagos.html'"><i class="fa-solid fa-file-invoice-dollar"></i></button>
-                        <button type="button" title="Abrir Limpieza" onclick="window.location.href='limpieza.html'"><i class="fa-solid fa-broom"></i></button>
+                        <button type="button" title="Abrir Pagos" onclick="event.stopPropagation(); if(window.location.pathname.includes('pagos.html')) { window.botInstance.closeAndClearChat(); } else { window.location.href='pagos.html'; }"><i class="fa-solid fa-file-invoice-dollar"></i></button>
+                        <button type="button" title="Abrir Limpieza" onclick="event.stopPropagation(); if(window.location.pathname.includes('limpieza.html')) { window.botInstance.closeAndClearChat(); } else { window.location.href='limpieza.html'; }"><i class="fa-solid fa-broom"></i></button>
                     </div>
                     <button class="bot-close" onclick="window.botInstance.closeAndClearChat()"><i class="fa-solid fa-xmark"></i></button>
                 </div>
