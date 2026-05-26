0dadb4f706b54dfa0c52c0a4a66e63e6daee7c5c
SimplexComunicación <96500851+sebadipy@users.noreply.github.com>
2026-05-26 18:50:01 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycby_j-CnF2qa09OygZmpU8QD0u6mZugysp31n_XsUZvLOlibgCp9W5ZCHenzDhgKpUTp/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxY81bPvyqSf5mV2WarWey3VbTk0Ot7HFe-wfhf4NkfHVeM9sbcSVXaM_uc_ubloHHN/exec"
 };
